1c8cf3e70aed1e9a6d27dc2872a44961f795858e
Robin Neumann <robin.neumann@posteo.de>
2019-10-31 16:13:57 +0100

    Support pie charts, too

--- lib/iruby/chartkick.rb
+++ lib/iruby/chartkick.rb
@@ -5,6 +5,7 @@ require "iruby/chartkick/version"
 require "iruby/chartkick/base_chart"
 require "iruby/chartkick/line_chart"
 require "iruby/chartkick/bar_chart"
+require "iruby/chartkick/pie_chart"
 
 module IRuby
   module Chartkick
@@ -24,6 +25,10 @@ module IRuby
 
     def bar_chart(data)
       BarChart.new(data).plot
+    end
+
+    def pie_chart(data)
+      PieChart.new(data).plot
     end
   end
 end
---
+++ spec/iruby/pie_chart_spec.rb
@@ -0,0 +1,24 @@
+﻿require "spec_helper"
+
+RSpec.describe IRuby::Chartkick::PieChart do
+  let(:data) do
+    {
+      2010 => 1,
+      2011 => 2,
+      2013 => 10
+    }
+  end
+
+  subject { described_class.new(data) }
+
+  describe "#plot" do
+    it "sends HTML to IRuby kernel" do
+      expect(::IRuby).to receive(:html).with(any_args).at_least(1)
+      subject.plot
+    end
+
+    it "knows how to construct custom randomized DOM id for chart canvas" do
+      expect(subject.randomized_dom_id).to start_with("iruby-chartkick-barchart")
+    end
+  end
+end
